e527d416ecbcd0a906f576a9252e42a8af157b63
Sergey <szaytsev060576@gmail.com>
2021-11-29 18:13:30 +0300

    создает карту фильтра

--- src/filter.js
+++ src/filter.js
@@ -1,5 +1,12 @@
 const filterMap = {
-  watchList: (film) => { },
-  history: (film) => { },
-  favorite: (film) => { },
+  addWatch: (films) => films.filter((film) => film.isAddedToWatch).length,
+  favorite: (films) => films.filter((film) => film.isFavorite).length,
+  watched: (films) => films.filter((film) => film.isWatched).length
 };
+
+export const generateFilter = (films) => Object.entries(filterMap).map(
+  ([filterName, countTasks]) => ({
+    name: filterName,
+    count: countTasks(films),
+  }),
+);
