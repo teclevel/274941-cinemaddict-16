bc3987846cafc1b6cd472ff1da103f5256c5d6a8
Sergey <szaytsev060576@gmail.com>
2021-12-10 17:59:38 +0300

    комментирует презентер

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,49 +1,70 @@
-import FilmsContainerView from './view/films-container-view';
-import FilmsBlockView from './view/films-block-view';
-import FilmsListContainerView from './view/films-list-container-view';
-import FilmsCardView from './view/films-card-view';
-import ButtonShowMoreView from './view/show-more-button-view';
-import FilmsPopupView from './view/films-popup-view';
-import CommentView from './view/comment-view';
-import { render, RenderPosition } from '../render';
+// import FilmsContainerView from './view/films-container-view';
+// import FilmsBlockView from './view/films-block-view';
+// import FilmsListContainerView from './view/films-list-container-view';
+// import FilmsCardView from './view/films-card-view';
+// import ButtonShowMoreView from './view/show-more-button-view';
+// import FilmsPopupView from './view/films-popup-view';
+// import CommentView from './view/comment-view';
+// import FilmsSortView from '../view/sort-view';
+// import { render, RenderPosition } from '../render';
+
+// const NUMBER_CARDS_PER_STEP = 5;
 
 
-export default class BoardPresenter {
-  #boardContainer = null;
+// export default class BoardPresenter {
+//   #boardContainer = null;
 
-  #filmsContainerComponent = new FilmsContainerView();
-  #filmsBlockComponent = new FilmsBlockView();
-  #filmsListContainerComponent = new FilmsListContainerView();
-  #filmsCardComponent = new FilmsCardView();
-  #filmsPopupComponent = new FilmsPopupView();
+//   #filmsContainerComponent = new FilmsContainerView();
+//   #filmsBlockComponent = new FilmsBlockView();
+//   #filmsListContainerComponent = new FilmsListContainerView();
+//   #filmsCardComponent = new FilmsCardView();
+//   #filmsPopupComponent = new FilmsPopupView();
+//   #filmsSortComponent = new FilmsSortView();
 
-  #boardCards = [];
+//   #boardCards = [];
 
-  constructor(boardContainer) {
-    this.#boardContainer = boardContainer;
-  }
+//   constructor(boardContainer) {
+//     this.#boardContainer = boardContainer;
+//   }
 
-  init = (boardCards) => {
-    this.#boardCards = [...boardCards];
-  }
+//   init = (boardCards) => {
+//     this.#boardCards = [...boardCards];
 
-  #renderFilmsContainer = () => {
+//     render(this.#boardContainer, this.#filmsContainerComponent, RenderPosition.BEFOREEND);
+//     render(this.#filmsCardComponent, FilmsBlockView, RenderPosition.BEFOREEND);
 
-  }
+//     this.#renderBoard();
+//   }
 
-  #renderFilmsBlock =()=>{
+//   #renderFilmsContainer = () => {
 
-  }
+//   }
 
-  #renderFilmsCard=()=>{
+//   #renderFilmsBlock = () => {
 
-  }
+//   }
 
-  #renderFilmsPopup=()=>{
+//   #renderFilmsCard = (from, to) => {
+//     this.#boardCards
+//       .slice()
+//       .forEach()
 
-  }
+//   }
 
-  #renderButtonShowMore=()=>{
+//   #renderFilmsPopup = () => {
 
-  }
-}
+//   }
+
+//   #renderButtonShowMore = () => {
+
+//   }
+
+//   #renderFilmsSort = () => {
+//     render(this.#filmsContainerComponent, this.#filmsSortComponent, RenderPosition.BEFOREBEGIN);
+
+//   }
+
+//   #renderBoard = () => {
+
+//   }
+// }
