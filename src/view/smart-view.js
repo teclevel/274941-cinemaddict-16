88bdd25a0c3b13fc0c5ca556a4c50e9922b81d3c
Sergey <szaytsev060576@gmail.com>
2021-12-23 16:43:25 +0300

    след заданию

--- src/view/smart-view.js
+++ src/view/smart-view.js
@@ -1,20 +1,20 @@
-import AbstractView from './abstract-view';
+// import AbstractView from './abstract-view';
 
-export default class SmartView extends AbstractView {
-  restoreHandler = () => {
-    // его нужно будет реализовать в наследнике.
-    //  Его задача — восстанавливать обработчики событий после перерисовки;
-  }
+// export default class SmartView extends AbstractView {
+//   restoreHandler = () => {
+//     // его нужно будет реализовать в наследнике.
+//     //  Его задача — восстанавливать обработчики событий после перерисовки;
+//   }
 
-  updateElement = () => {
-    // удалить старый DOM-элемент компонента;
-    // создать новый DOM-элемент;
-    // поместить новый элемент вместо старого;
-    // восстановить обработчики событий, вызвав restoreHandlers
-  }
+//   updateElement = () => {
+//     // удалить старый DOM-элемент компонента;
+//     // создать новый DOM-элемент;
+//     // поместить новый элемент вместо старого;
+//     // восстановить обработчики событий, вызвав restoreHandlers
+//   }
 
-  updateData = () => {
-    // будет обновлять данные и, если нужно, вызывать метод updateElement.
-  }
+//   updateData = () => {
+//     // будет обновлять данные и, если нужно, вызывать метод updateElement.
+//   }
 
-}
+// }
